0946fe272bfbbc04a31f6ced8114aa9e5699c84b
meta-sankalp-vyas <41181605+meta-sankalp-vyas@users.noreply.github.com>
2021-05-15 05:35:38 +0530

    Extract Data Functionality Added. (#3)

--- databaseutil.py
+++ databaseutil.py
@@ -9,30 +9,56 @@ from utility import sqlcommandsphrases as SQLCommandsPhrases
 
 class databaseutil:
 
+	def createSchema(self, dbConnection):
+		utility = Utility()
+		self.createDataTablesSQLScript(SQLCommandsPhrases.SchemaName.value)
+		sqlRead = FileUtil(ResourceLocation.DatabaseScript.value, "r")
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, "", LogMessage.DBDatabaseCreation.value,"a", False)		
+		self.executeAndCommitToDatabase(dbConnection, sqlRead)
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, "", LogMessage.Completed.value,"a", True)
+
+	def loadDataFromCSV(self, dbConnection, schemaName):
+		utility = Utility()
+		fileList = os.listdir(ResourceLocation.DatabaseLocation.value)
+		filePaths = self.getFilePaths(fileList)
+		insertSQLs = []
+		for filePath in filePaths:
+			file = (FileUtil(filePath, "r")).getFile()
+			dbConnection.copyFromCSVs(file, schemaName + "." + (((filePath.split("/"))[2]).split("."))[0], ",")
+			dbConnection.commitTransaction()
+		dbConnection.closeDBConnection()
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, "", LogMessage.Completed.value,"a", True)
+		print("Loading Data")
+
 	def createDataTablesSQLScript(self, schemaName):
 		utility = Utility()
 		fileList = os.listdir(ResourceLocation.DatabaseLocation.value)
 		tableHeaders = self.getTableHeader(fileList)
 		createTableCommands = self.createCommandsFromHeaders(tableHeaders, fileList, schemaName)
-		utility.writeLogs(ResourceLocation.LogFileLocation.value, ("\n").join(createTableCommands), LogMessage.DBDatabaseCreationCommands.value,"a")
-		self.writeCommandSqlScript(("\n").join(createTableCommands), ResourceLocation.DatabaseScript.value)
-		utility.writeLogs(ResourceLocation.LogFileLocation.value, "", LogMessage.DBDatabaseSQLScriptCreated.value,"a")
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, ("\n").join(createTableCommands), LogMessage.DBDatabaseCreationCommands.value,"a", False)
+		utility.writeCommandSqlScript(("\n").join(createTableCommands), ResourceLocation.DatabaseScript.value)
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, "", LogMessage.DBDatabaseSQLScriptCreated.value,"a", False)
 
 	def getTableHeader(self, fileList):
 		utility = Utility()
-		filePaths = []
-		for fileName in fileList:
-			if "csv" in fileName: 
-				filePaths.append(ResourceLocation.DatabaseLocation.value + fileName)
-		utility.writeLogs(ResourceLocation.LogFileLocation.value, ("\n").join(filePaths), LogMessage.Files.value,"a")
+		filePaths = self.getFilePaths(fileList)
+		utility.writeLogs(ResourceLocation.LogFileLocation.value, ("\n").join(filePaths), LogMessage.Files.value,"a", False)
 		tableHeaders = []
 		for filePath in filePaths:
 			fileHeader = ((FileUtil(filePath, "r")).getFileContent())[0]
 			tableHeaders.append(fileHeader)
 		return tableHeaders
+
+	def getFilePaths(self, fileList):
+		filePaths = []
+		for fileName in fileList:
+			if "csv" in fileName: 
+				filePaths.append(ResourceLocation.DatabaseLocation.value + fileName)
+		return filePaths
 		
 	def createCommandsFromHeaders(self, tableHeaders, fileList, schemaName):
 		createTableCommands = []
+		createTableCommands.append(SQLCommandsPhrases.DropSchema.value + " " + schemaName + SQLCommandsPhrases.Cascade.value + ";\n")
 		createTableCommands.append(SQLCommandsPhrases.CreateSchema.value + " " + schemaName + ";\n")
 		tableIndex = 0
 		for tableHeader in tableHeaders:
@@ -52,10 +78,11 @@ class databaseutil:
 			createTableCommands.append(createCommand)
 			tableIndex += 1
 		return createTableCommands
-	
-	def writeCommandSqlScript(self, content, fileLocation):
-		sqlScript = FileUtil(ResourceLocation.DatabaseScript.value, "w")
-		sqlScript.writeFileContect(content)
+
+	def executeAndCommitToDatabase(self, dbConnection, sqlRead):
+		dbConnection.executeSQL(sqlRead.readFile())
+		dbConnection.commitTransaction()
+		dbConnection.closeDBConnection()
 
 
 
