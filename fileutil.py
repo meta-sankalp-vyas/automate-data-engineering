0e077ff43ad52a1d35c76bf5dc0a7bd7ffe010c1
meta-sankalp-vyas <41181605+meta-sankalp-vyas@users.noreply.github.com>
2021-05-19 19:13:59 +0530

    Feature/exception handlings (#9)
    * Exception Handling feature added.
    
    * Exception Handling RollBack. CSV read from CSV for the configuration. UTF-8 used as default reading encoding.

--- fileutil.py
+++ fileutil.py
@@ -1,3 +1,4 @@
+import csv
 class fileutil:
 	def __init__(self,fileLocation,access):
 		self.fileLocation=fileLocation
@@ -10,20 +11,33 @@ class fileutil:
 		return self.fileAccess
 
 	def getFileContent(self):
-		readFileConfiguration = open(self.fileLocation,self.fileAccess)
+		readFileConfiguration = open(self.fileLocation,self.fileAccess,encoding="utf8")
 		fileContent = readFileConfiguration.readlines()
 		return fileContent
 
 	def writeFileContent(self, content):
-		writeFileConfiguration = open(self.fileLocation,self.fileAccess)
+		writeFileConfiguration = open(self.fileLocation,self.fileAccess,encoding="utf8")
 		writeFileConfiguration.write(content)
 		writeFileConfiguration.close()
 		return 1>0
 	
 	def readFile(self):
-		readFileConfiguration = open(self.fileLocation,self.fileAccess)
+		readFileConfiguration = open(self.fileLocation,self.fileAccess,encoding="utf8")
 		fileContent = readFileConfiguration.read()
 		return fileContent
 
 	def getFile(self):
-		return open(self.fileLocation,self.fileAccess)
+		return open(self.fileLocation,self.fileAccess,encoding="utf8")
+
+	def getCSVReader(self):
+		csvReader = None
+		returnArray = []
+		with open(self.fileLocation) as csv_file:
+			csvReader = csv.reader(csv_file, delimiter=',', quotechar='|')
+			line_count = 0
+			for row in csvReader:
+				returnArray.append("|,|".join(row))
+				line_count += 1
+		return returnArray
+
+		
